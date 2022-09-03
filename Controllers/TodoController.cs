3eb00540453b8ed00e423f6fe97995729f62ebeb
Thiago <thiagofp707@gmail.com>
2022-09-03 20:01:55 -0300

    Criando Api e modelo de dados

---
+++ Controllers/TodoController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyTodo.Models;
+
+namespace MyTodo.Controllers;
+
+[ApiController]
+[Route("v1")]
+public class TodoController : ControllerBase
+{
+    [HttpGet]
+    [Route("todos")]
+    public List<Todo> Get()
+    {
+        return new List<Todo>();
+    }
+}
---
+++ Migrations/20220903222341_AddedDateTime.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyTodo.Migrations
+{
+    public partial class AddedDateTime : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Date",
+                table: "Todos",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Date",
+                table: "Todos");
+        }
+    }
+}
